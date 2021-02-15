d96b6cca585011db30eb75476b9a690bb5cb7680
OneJaredNewman <56051400+OneJaredNewman@users.noreply.github.com>
2021-02-15 17:16:36 -0500

    Update youtube.js

--- youtube.js
+++ youtube.js
@@ -1,1 +1,1 @@
-javascript:(function(){var l=location;l.href=l.origin.replace('m.youtube.com', 'inv.skyn3t.in')+l.href.replace(l.origin,'');})();
+javascript:location.hostname.includes("youtube.com")?location.assign("https://invidious.xyz"+location.pathname+location.search+location.hash):location.assign("https://invidious.xyz");
